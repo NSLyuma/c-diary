abcdd0ab5299b93ab6e0ae92926b1374dbada82e
Anastasiya <iinokayo@mail.ru>
2023-03-25 23:19:10 +0300

    Changed migrations

--- backend/db/migrations/20230325200955-create-product.js
+++ backend/db/migrations/20230325200955-create-product.js
@@ -1,4 +1,3 @@
-'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -7,25 +6,28 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.TEXT
+        allowNull: false,
+        unique: true,
+        type: Sequelize.TEXT,
       },
       kcalOn100g: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable('Products');
-  }
+  },
 };
