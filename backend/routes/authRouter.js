53bb7e8197b2ed440e8339b853bc49f95bafb58d
Anastasiya <iinokayo@mail.ru>
2023-03-25 23:43:12 +0300

    Added auth/login route

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -2,6 +2,65 @@ const authRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../db/models');
 
+authRouter.post('/login', async (req, res) => {
+  // достаём почту и пароль пользователя
+  const isEmail = Boolean(req.body.email.trim());
+  const isPassword = Boolean(req.body.password.trim());
+
+  // проверка, что пользователь ввёл и почту, и пароль
+  if (!isEmail || !isPassword) {
+    res.status(403).json({ error: 'Необходимо ввести email и пароль!' });
+    return;
+  }
+
+  // создаём пользователя
+  let user;
+
+  // достаём его из БД по email
+  try {
+    user = await User.findOne({
+      where: { email: req.body.email },
+    });
+  } catch (error) {
+    console.log(`Ошибка при авторизации: ${error.message}`);
+    res.status(500).json({ error: 'Ошибка сервера' });
+    return;
+  }
+
+  // если такого пользователя нет в БД, отправляем ошибку
+  if (!user) {
+    res.status(403).json({ error: 'Неверный email или пароль!' });
+    return;
+  }
+
+  // достаём сырой и хэшированный пароли
+  const rawPassword = req.body.password;
+  const hashedPassword = user.password;
+  let isSame;
+
+  // сравниваем пароли
+  try {
+    isSame = await bcrypt.compare(rawPassword, hashedPassword);
+  } catch (error) {
+    console.log(`Ошибка при сравнении паролей: ${error.message}`);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+
+  // если пароли не совпадают, отправляем ошибку
+  if (!isSame) {
+    res.status(403).json({ error: 'Неверный email или пароль!' });
+    return;
+  }
+
+  // если всё хорошо, создаём сессию
+  req.session.userId = user.id;
+
+  res.json({
+    userName: user.name,
+    role: user.role,
+  });
+});
+
 authRouter.post('/register', async (req, res) => {
   // проверка на длину имени
   if (req.body.userName.trim().length < 2) {
